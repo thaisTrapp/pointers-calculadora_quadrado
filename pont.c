81a93d9e5602c4f2994bcc7cd40a5186a99ecf02
thaisTrapp <113135784+thaisTrapp@users.noreply.github.com>
2023-01-16 23:26:41 -0300

    ponteiros

---
+++ pont.c
@@ -0,0 +1,33 @@
+/****************************************************************************
+ ponteiros swap
+*****************************************************************************/
+   
+#include <stdio.h>
+
+void quadrado ();
+ 
+int main () 
+{
+  
+ 
+int a;
+
+    printf("insira valor para receber o quadrado dele: ");
+    scanf("%i",&a);
+    printf("\n quadrado de %i =",a);
+    
+    quadrado(&a);
+
+    printf(" %i ",a);
+ 
+} 
+
+void quadrado (int *p1){
+    
+   *p1 = (*p1)*(*p1);
+    
+    
+    
+}
+
+
